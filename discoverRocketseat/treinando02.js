dc69b0bc67cdf003b487a5feae7c463515ea308c
Lameque Fernandes <lamequesao@gmail.com>
2021-06-18 15:33:55 -0300

    scope let e const

--- discoverRocketseat/treinando02.js
+++
@@ -1,9 +0,0 @@
-//console.log("> existe y antes do bloco? ", y); erro, y n esta visivel
-
-{
-// console.log("> existe y antes do bloco? ", y); erro n conseguiu acessar y
-   let y = 1 // penas funciona no escopo, local
-   console.log("> existe y? ", y);
-}
-
-// console.log("> existe y antes do bloco? ", y); erro, fora do escopo
---
+++ discoverRocketseat/treinandoLetAndConst.js
@@ -0,0 +1,18 @@
+//console.log("> existe y antes do bloco? ", y); erro, y n esta visivel
+const x = 5;
+
+{
+// console.log("> existe y antes do bloco? ", y); erro n conseguiu acessar y
+   let y = 1; // penas funciona no escopo, local
+   console.log("> existe y? ", y);
+}
+
+// x = 10; erro, const n pode mudar de valor
+// console.log("> existe y antes do bloco? ", y); erro, fora do escopo
+
+let y = 0;
+{
+      y = 1; // penas funciona no escopo, local
+      console.log("> existe y? ", y);
+}
+console.log("> existe y? ", y);
