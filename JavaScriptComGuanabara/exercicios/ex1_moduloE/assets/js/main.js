f33bf11e039d0992bfca2209b9c49b0093185b89
Lameque Fernandes <lamequesao@gmail.com>
2021-11-12 21:51:44 -0300

    exercicio contador js com html e css

---
+++ JavaScriptComGuanabara/exercicios/ex1_moduloE/assets/js/main.js
@@ -0,0 +1,20 @@
+var t_inicio = document.querySelector('input#inicio');
+var t_fim = document.querySelector('input#fim');
+var t_passos = document.querySelector('input#n_passos')
+var botao_contar = document.querySelector('input#botao_contar');
+
+var div_result = document.querySelector('div#result');
+
+botao_contar.addEventListener('click', click_botao);
+
+function click_botao() {
+    var num_inicio = Number(t_inicio.value);
+    var num_fim = Number(t_fim.value);
+    var num_passos = Number(t_passos.value);
+
+    var tagP = document.createElement('p')
+
+    for (var i = num_inicio; i <= num_fim; i += num_passos) {
+        div_result.innerHTML += `${i}  `
+    }
+}
